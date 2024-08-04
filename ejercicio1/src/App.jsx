c4116f5ad25b710643bbd2facba29cdc52435cbb
Carlos Trujillo <carlos.trujillo.bazante270494@gmail.com>
2024-08-04 18:34:58 -0500

    ejercicio 1

--- ejercicio1/src/App.jsx
+++ ejercicio1/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
+  const calculateDiscount = (price, discount) => {
+    return price * ((100 - discount) / 100);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Price Discount Calculator</h1>
+      <p>Original Price: $100</p>
+      <p>Discount: 20%</p>
+      <p>Original Price: $ {calculateDiscount(100, 20)}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
