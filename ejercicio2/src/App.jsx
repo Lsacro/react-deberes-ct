18fe342a159e0f4d9db136b968d02b249058a9b0
Carlos Trujillo <carlos.trujillo.bazante270494@gmail.com>
2024-08-04 19:31:23 -0500

    ejercicio3

--- ejercicio2/src/App.jsx
+++ ejercicio2/src/App.jsx
@@ -1,5 +1,14 @@
-import { UserProfile } from "./components/UserProfile";
 function App() {
+  const UserProfile = ({ name, age, mail }) => {
+    return (
+      <>
+        <h1>User Profile</h1>
+        <p>Name: {name}</p>
+        <p>Age: {age}</p>
+        <p>Email: {mail}</p>
+      </>
+    );
+  };
   return <UserProfile name={"Joel"} age={30} mail={"carlos@gmail.com"} />;
 }
 
