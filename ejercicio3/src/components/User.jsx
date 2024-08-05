13cb7e139db9cad445fce3dda698331d0aa02d19
Carlos Trujillo <carlos.trujillo.bazante270494@gmail.com>
2024-08-04 19:36:46 -0500

    ejercicio3

---
+++ ejercicio3/src/components/User.jsx
@@ -0,0 +1,10 @@
+export function User({ name, age, mail }) {
+  return (
+    <>
+      <h1>User Profile</h1>
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+      <p>Email: {mail}</p>
+    </>
+  );
+}
